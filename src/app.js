7b95b81d4eb3979d89661ed30c8e36e74fa0ad78
salik beigh <beighsalik51@gmail.com>
2025-08-15 16:35:14 +0530

    test

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 //  req handler
 app.use((req, res) => {
   if (req.url === "/hello") {
-    res.send("asad malla asad noob hello ehy");
+    res.send("asad malla asad noob hello ");
   } else {
     res.send("asad malla");
   }
