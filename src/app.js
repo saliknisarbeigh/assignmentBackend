95400e68dc8a98d39364488a7250f1793a70e2e6
salik beigh <beighsalik51@gmail.com>
2025-08-20 21:40:16 +0530

    updated schema

--- src/app.js
+++ src/app.js
@@ -1,7 +1,82 @@
 const express = require("express");
 const app = express();
 const connectDb = require("./config/dataBase");
+const User = require("./models/user");
+const { ReturnDocument } = require("mongodb");
+app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
+
+  await user.save();
+  res.send("user added");
+});
+app.use("/check", async (req, res) => {
+  try {
+    const userById = await User.findById("68a1b8541480f319a18fcf4d");
+    res.send(userById);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+app.use("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.find({ email: userEmail });
+
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({ email: userEmail });
+
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.delete("/delete", async (req, res) => {
+  try {
+    const userId = "68a2c18035a827cf3d78146d";
+
+    const user = await User.findByIdAndDelete(userId);
+    res.send("deleted successfully");
+    console.log(user);
+  } catch (err) {
+    res.status(400).send("practice karle laadle");
+  }
+});
+
+app.patch("/update", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    res.send("update successfully");
+  } catch (err) {
+    res.status(400).send("update failed: " + err.message);
+  }
+});
 connectDb()
   .then(() => {
     console.log(" dataBase connection successful");
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,54 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      unique: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/thumbnails/005/544/770/small/profile-icon-design-free-vector.jpg",
+    },
+    about: {
+      type: String,
+      default: "hey there this is a default value",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
-const UserModel = mongoose.model("user", UserSchema);
-module.exports = UserModel;
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("user", UserSchema);
